84a00f1e759273533e50cf6310023a8980f55c11
maxschramp <maxschramp@gmail.com>
2024-04-02 10:51:26 -0400

    Add files via upload

---
+++ py-setup project here.bat
@@ -0,0 +1,27 @@
+@echo on
+
+echo Creating requirements.txt...
+type nul > requirements.txt
+
+echo Creating virtual environment...
+python -m venv venv
+
+echo Activating virtual environment...
+call .\venv\Scripts\activate.bat
+
+echo Updating pip...
+python -m pip install -U pip
+
+:: echo Installing pip pre-built packages...
+pip install -U wheel
+
+echo Installing pip requirements...
+pip install -Ur requirements.txt
+
+echo Creating main.py...
+
+
+echo Creating main.bat...
+
+
+pause
