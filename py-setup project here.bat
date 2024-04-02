76409e5d0ce15ab67473debcef3b83e4de94f33c
maxschramp <maxschramp@gmail.com>
2024-04-02 11:03:38 -0400

    Update py-setup project here.bat

--- py-setup project here.bat
+++ py-setup project here.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 
 echo Creating requirements.txt...
 type nul > requirements.txt
@@ -18,10 +18,14 @@ pip install -U wheel
 echo Installing pip requirements...
 pip install -Ur requirements.txt
 
-echo Creating main.py...
-
-
 echo Creating main.bat...
+curl -o main.bat https://raw.githubusercontent.com/maxschramp/python-project-template/aa729210ff196f8d16c75e518221a0613027a629/main.bat
 
+echo Creating template.py...
+curl -o template.py https://raw.githubusercontent.com/maxschramp/python-project-template/aa729210ff196f8d16c75e518221a0613027a629/template.py
+
+echo Creating main.py
+
+template.py > main.py
 
 pause
