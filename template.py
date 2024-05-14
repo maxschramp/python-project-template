1873f193ee8218d8216a49b318870f0bb65316d0
maxschramp <maxschramp@gmail.com>
2024-05-14 16:20:45 -0400

    Update template.py

--- template.py
+++ template.py
@@ -1,6 +1,9 @@
 #!.\venv\Scripts\python.exe
 # template courtesy of https://adamj.eu/tech/2021/10/09/a-python-script-template-with-and-without-type-hints-and-async/
 
+from colorama import Fore, Style
+from dotenv import dotenv_values
+
 from __future__ import annotations
 
 import argparse
@@ -13,8 +16,12 @@ def main(argv: Sequence[str] | None = None) -> int:
     # parser.add_argument("--name", type=str, required=True, nargs=1)
     args = parser.parse_args(argv)
 
+    # load variables from .env file
+    global env; env = dotenv_values(f"{os.path.dirname(os.path.realpath(__file__))}\\.env")
+    
     # Start writing code here
-
+    
+    print(Style.RESET_ALL)
     return 0
 
 
